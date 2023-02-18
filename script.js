30527b47588d0e6f226e22f251fce75754ed0626
Ana Silveira <ana.ccord@gmail.com>
2023-02-18 08:54:58 -0300

    chore: hiding and handling cards. Mechanics are ok.

--- script.js
+++ script.js
@@ -1,62 +1,72 @@
 
-let cardList = ['1','2','3','4','5','6','1','2','3','4','5','6']
-let cards = document.querySelectorAll('.card')
+let cardList = [1, 2, 3, 4, 5, 6];
+let cards = document.querySelectorAll('.card');
 
-let card1 = 0
-let card2 = 0
+let currentRandomIndexes = [];
+let card1 = 0;
+let card2 = 0;
 
+function getRandomCardIndexes() {
+  const randomIndex = Math.floor(Math.random() * cardList.length);
+  const hasIndexPair = currentRandomIndexes.filter((index) => index === randomIndex).length >= 2;
 
-function setCards() {  
+  if (hasIndexPair) {
+    return getRandomCardIndexes();
+  }
 
-    for (var i = 0; i < cards.length; i++) {
-        const randomNumber = Math.floor(Math.random() * cardList.length);
-        cards[i].innerText = cardList[randomNumber];
-        cardList = cardList.splice(randomNumber, 1)
-        // more statements
-     }
+  currentRandomIndexes.push(randomIndex);
 
+  return randomIndex;
 }
 
+function setCards() {
+  cards.forEach((card) => {
+    const randomIndex = getRandomCardIndexes();
+    card.innerText = cardList[randomIndex];
+  });
+}
 
-setCards()
+setCards();
 
 console.log("cardList" + cardList);
 console.log("cards" + cards);
 
-
-
 cards.forEach(card => {
-    card.addEventListener('click', () => compareCards(card))    
+  card.addEventListener('click', () => compareCards(card));
 });
 
 function compareCards(card) {
-    if (card.classList.contains("isSameContent")){
-        return
-    }
-     const content = card;
-    if (card1 === 0) {
-        card1 = content;
-        content.classList.add("isSameContent");
-    } else if (card2 === 0) {
-       card2 = content;
-       content.classList.add("isSameContent");
-    (card1.innerText === card2.innerText) ? matchThis(card1,card2) : cleanThis(card1,card2) 
-    
-    } 
-     
+  if (card.classList.contains("isSameContent")){
+      return
+  }
+   const content = card;
+  if (card1 === 0) {
+      card1 = content;
+      card1.classList.add("isSameContent");
+  } else if (card2 === 0) {
+     card2 = content;
+     card2.classList.add("isSameContent");
+  (card1.innerText === card2.innerText) ? 
+  matchThis(card1,card2) :  
+  cleanThis(card1,card2);  
+  
+  } 
+   
 }
 
-
-function matchThis(a, b) {    
-    console.log("It's a Match"+ a.innerText, b.innerText)
-    card1 = 0
-    card2 = 0
+function matchThis(a, b) {
+  console.log("It's a Match" + a.innerText, b.innerText);
+  card1 = 0;
+  card2 = 0;
 }
 
 function cleanThis(a, b) {
-    a.classList.remove("isSameContent");
+  card1 = 0;
+  card2 = 0;
+  console.log("Not Now" + a.innerText, b.innerText);
+
+  setTimeout(() => {  
     b.classList.remove("isSameContent");
-    card1 = 0
-    card2 = 0    
-    console.log("Not Now" + a.innerText, b.innerText)
+    a.classList.remove("isSameContent");
+}, 500);
 }
